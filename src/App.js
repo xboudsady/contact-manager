73fc406b1c4bc64788ab99660b2f74038ab02177
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-08-01 06:59:33 -0700

    Created Header components in /components/Header.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import "./App.css";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>The App Component</h1>
+                <Header />
                 <Contact />
             </div>
         );
