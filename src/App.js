6c0e73483f189f27b4bb4685a617b59dfd216e4e
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-08-01 06:53:34 -0700

    Created components folder, with contacts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import Contact from "./components/Contact";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>The App Component</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>The App Component</h1>
+                <Contact />
+            </div>
+        );
+    }
 }
 
 export default App;
