a3fd988a2dfde95344cd6eb31a19ed4315aa7d73
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-08-02 20:45:34 -0700

    Font awesome added, and add event listener to icon.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,21 @@ import PropTypes from "prop-types";
 
 // This is a class component not functional component, we have to use 'this.props' instead of just props
 class Contact extends Component {
+    state = {};
+
+    onShowClick = e => {};
+
     render(props) {
         const { name, email, phone } = this.props.contact; // We can extract value out of props, so we don't need to pass in 'this.props' each time
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>
+                    {name}{" "}
+                    <i
+                        onClick={this.onShowClick}
+                        className="fas fa-sort-down"
+                    />
+                </h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
